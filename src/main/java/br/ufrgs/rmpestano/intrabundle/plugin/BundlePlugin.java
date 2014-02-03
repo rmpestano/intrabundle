21f68626c6c7524d1ccd3d8ca79b4acfb282f8e5
rmpestano <rmpestano@gmail.com>
2014-02-03 12:41:31 -0200

    - bundle plugin commands

--- src/main/java/br/ufrgs/rmpestano/intrabundle/plugin/BundlePlugin.java
+++ src/main/java/br/ufrgs/rmpestano/intrabundle/plugin/BundlePlugin.java
@@ -1,11 +1,12 @@
 package br.ufrgs.rmpestano.intrabundle.plugin;
 
 import br.ufrgs.rmpestano.intrabundle.facet.BundleFacet;
+import br.ufrgs.rmpestano.intrabundle.i18n.ResourceBundle;
 import br.ufrgs.rmpestano.intrabundle.model.OSGiModule;
-import br.ufrgs.rmpestano.intrabundle.model.OSGiModuleImpl;
 import org.jboss.forge.shell.ShellPrompt;
 import org.jboss.forge.shell.plugins.*;
 
+import javax.enterprise.inject.Instance;
 import javax.inject.Inject;
 
 /**
@@ -20,14 +21,51 @@ public class BundlePlugin implements Plugin {
     @Inject
     OSGiModule bundle;
 
+    @Inject
+    @br.ufrgs.rmpestano.intrabundle.annotation.Current
+    Instance<ResourceBundle> resourceBundle;
 
 
     @DefaultCommand
     public void defaultCommand(@PipeIn String in, PipeOut out) {
-        out.println(((OSGiModuleImpl)bundle).getProjectRoot().toString());
+        out.println("default command");
     }
 
-    @Command(help = "true if bundles exported packages contains only interfaces, false if it contains one or more classes")
+    @Command
+    public void usesDeclarativeServices(@PipeIn String in, PipeOut out) {
+           out.println(bundle.getUsesDeclarativeServices().toString());
+    }
+
+    @Command(help = "prints module activator path")
+    public void activator(@PipeIn String in, PipeOut out) {
+            out.println((bundle.getActivator() != null ? bundle.getActivator().getFullyQualifiedName() : resourceBundle.get().getString("osgi.no-activator")));
+    }
+
+    @Command(help = "list bundle exported packages")
+    public void exportedPackages(PipeOut out){
+        if(bundle.getExportedPackages().isEmpty()){
+            out.println(resourceBundle.get().getString("module.noExportedPackages"));
+        }
+        else{
+            for (String s : bundle.getExportedPackages()) {
+                out.println(s);
+            }
+        }
+    }
+
+    @Command(help = "list bundle imported packages")
+    public void importedPackages(PipeOut out){
+        if(bundle.getImportedPackages().isEmpty()){
+            out.println(resourceBundle.get().getString("module.noImportedPackages"));
+        }
+        else{
+            for (String s : bundle.getImportedPackages()) {
+                out.println(s);
+            }
+        }
+    }
+
+    @Command(help = "true if bundle exported packages contains only interfaces, false if it contains one or more classes")
     public void publishesInterfaces(@PipeIn String in, PipeOut out, @Option String... args) {
         out.println(bundle.getPublishesInterfaces().toString());
     }
